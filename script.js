6d177d7ba6387d330b91f2336f2f923a4ca89dfe
Leandro Veliz <leanvelizcrc@gmail.com>
2022-09-12 01:07:56 -0300

    se agrega detalles en js y index

--- script.js
+++ script.js
@@ -79,3 +79,27 @@ let interval = setInterval(function(){
                     }
                   );
                 });
+//Audio
+const cargarSonido = function (fuente) {
+  const sonido = document.createElement("audio");
+  sonido.src = fuente;
+  sonido.setAttribute("preload", "auto");
+  sonido.setAttribute("controls", "none");
+  sonido.style.display = "none"; // <-- oculto
+  document.body.appendChild(sonido);
+  return sonido;
+};
+const $botonReproducir = document.querySelector("#btnReproducir"),
+  $botonPausar = document.querySelector("#btnPausar"),
+  $botonReiniciar = document.querySelector("#btnReiniciar");
+// El sonido que podemos reproducir o pausar
+const sonido = cargarSonido("./audio/All of Me  John Legend.mp3");
+$botonReproducir.onclick = () => {
+  sonido.play();
+};
+$botonPausar.onclick = () => {
+  sonido.pause();
+};
+$botonReiniciar.onclick = () => {
+  sonido.currentTime = 0;
+};
